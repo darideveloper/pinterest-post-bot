1c3c579d4addcddf2b30b9cc1d1e6162eb1bd569
daridev <darideveloper@gmail.com>
2023-12-12 13:56:03 -0600

    feat (canva/create_ad_1): replace texts & replace image & save changes

--- libs/canva.py
+++ libs/canva.py
@@ -33,6 +33,33 @@ class Canva ():
         if login_text.lower().strip() == "log in":
             logger.error("\tERROR: You should login manually to canva")
             quit()
+            
+    def __insert_innerhtml__ (self, selector, inner_html):
+        """ Insert inner html in element
+        
+        Args:
+            selector (str): Selector of element
+            inner_html (str): Inner html to insert
+        """
+        
+        script = f"""document.querySelector ('{selector}').innerHTML = `{inner_html}`"""
+        self.scraper.driver.execute_script(script)
+    
+    def __select_layer__ (self, layer_index: int):
+        """ Select specific layer in canva aside
+
+        Args:
+            layer_index (int): Index of layer to select
+        """
+        
+        selector_show_layers = 'main div:nth-child(1) button'
+        selector_layer = f'div:nth-child({layer_index}) > div:nth-child(3) button'
+        
+        
+        self.scraper.click_js(selector_show_layers)
+        self.scraper.refresh_selenium()
+        self.scraper.click_js(selector_layer)
+        self.scraper.refresh_selenium()
         
     def remove_bg_image(self, image_path: str) -> str:
         """ Remove background image from canva
@@ -84,8 +111,113 @@ class Canva ():
         image_path = os.path.join(self.images_folder, new_image)
         return image_path
     
-    def create_ad(self):
-        pass
+    def create_ad_1(self, add_link: str, title: str, price_1: float, 
+                    price_2: float, price_3: float, price_4: float,
+                    image_path: str):
+        """ Replace data in add template 1, and download image
+        
+        Args:
+            add_link (str): Link of add template
+            title (str): Title of add
+            price_1 (float): Cheaper price
+            price_2 (float): 2nd cheaper price
+            price_3 (float): 3rd cheaper price
+            price_4 (float): 4th cheaper price
+            image_path (str): Path of image to upload
+        """
+        
+        selectors = {
+            # ad texts
+            "title": 'div:nth-child(9) p.cgHgbA',
+            "price_1": 'div:nth-child(8) p',
+            "price_2": 'div:nth-child(5) p.cgHgbA',
+            "price_3": 'div:nth-child(5) p.cgHgbA:nth-child(2)',
+            "price_4": 'div:nth-child(5) p.cgHgbA:nth-child(3)',
+            
+            # Ad image
+            'delete_btn': 'div.awStMQ div:nth-child(2) > button',
+            'files_tab': 'aside div:nth-child(6) > button',
+            'input_image': 'input[type="file"]',
+            'insert_image_btn': 'div.x6XCCg div:nth-child(2) > div.D_ZUcw div[role="button"]',
+            
+            # scale and position image
+            'organize_tab': 'div.gezMZQ button.zCtFuA',
+            'lock_relation_btn': 'div.Ey7S7w button',
+            'height_input': 'div.Ey7S7w > div:nth-child(2) input',
+            'center_x_btn': 'div:nth-child(3) > ul > li:nth-child(4) > button',
+            'center_y_btn': 'div:nth-child(3) > ul > li:nth-child(3) > button',            
+            
+            # Save and download
+            'file_tab': '[aria-controls="header_item5"]',
+            'save_btn': 'li:nth-child(9) > button',
+            "download_btn": 'div:nth-child(9) button:last-child',
+        }
+        
+        self.scraper.set_page(add_link)
+        self.scraper.refresh_selenium()
+        
+        # Replace titlle
+        html = f"""<span class="OYPEnA" style="font-weight: 400; font-style: normal;
+                color: rgb(82, 113, 255); text-decoration: none;">{title}</span>"""
+        self.__insert_innerhtml__ (selectors['title'], html)
+        sleep(2)
+        
+        # Replace cheaper price
+        html = f"""<span class="OYPEnA" style="font-weight: 400; font-style: normal; 
+               color: rgb(54, 48, 98); text-decoration: none;">Cheaper: </span><span 
+               class="OYPEnA" style="font-weight: 400; font-style: normal; color: 
+               rgb(82, 113, 255); text-decoration: none;">US ${price_1}</span>"""
+        self.__insert_innerhtml__ (selectors['price_1'], html)
+        sleep(2)
+
+
+        # Replace 2nd, 3rd and 4th cheaper prices
+        seconday_prices = [
+            ["2nd", price_2, selectors['price_2']],
+            ["3rd", price_3, selectors['price_3']],
+            ["4th", price_4, selectors['price_4']],
+        ]
+        
+        for secondary_price in seconday_prices:
+            position, price, selector = secondary_price
+        
+            # Replace price
+            html = f"""<span class="OYPEnA" style="font-weight: 400; font-style: normal; color: 
+            rgb(54, 48, 98); text-decoration: none;">{position} place: </span><span class="OYPEnA" 
+            style="font-weight: 400; font-style: normal; color: rgb(82, 113, 255); 
+            text-decoration: none;">US ${price}</span>"""
+            self.__insert_innerhtml__ (selector, html)
+            sleep(2)
+        
+        
+        # Delete image
+        self.__select_layer__(5)
+        self.scraper.click_js(selectors["delete_btn"])
+        self.scraper.refresh_selenium()
+        
+        # Move to files tab
+        self.scraper.click_js(selectors["files_tab"])
+        self.scraper.refresh_selenium()
+        
+        # Upload new image
+        self.scraper.send_data(selectors["input_image"], image_path)
+        sleep(5)
+        self.scraper.refresh_selenium()
+        self.scraper.click_js(selectors["insert_image_btn"])
+        
+        # Scale image
+        self.__select_layer__(5)
+        
+        # Center image
+        
+        # Download image
+        
+        # Save changes
+        self.scraper.click_js(selectors["file_tab"])
+        self.scraper.refresh_selenium()
+        self.scraper.click_js(selectors["save_btn"])
+        sleep(3)
+        print ()
 
 
 if __name__ == "__main__":
@@ -96,5 +228,16 @@ if __name__ == "__main__":
         download_folder=images_folder
     )
     canva = Canva(scraper)
-    image_path = os.path.join(images_folder, "sample.jpg")
-    canva.remove_bg_image(image_path)
+    image_path = os.path.join(images_folder, "sample.webp")
+    
+    # canva.remove_bg_image(image_path)
+    
+    canva.create_ad_1(
+        add_link="https://www.canva.com/design/DAF2ygpQ0rM/g4ZdDtW41Z-oQD8sUwL4Yw/edit",
+        title="Sample title",
+        price_1=1.99,
+        price_2=2.99,
+        price_3=3.99,
+        price_4=4.99,
+        image_path=image_path,
+    )
