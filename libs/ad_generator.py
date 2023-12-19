4c437e3683a0302ecfeb6f4d195f58bf8b1caab1
daridev <darideveloper@gmail.com>
2023-12-19 12:37:21 -0600

    feat (libs): create module ad_generator

---
+++ libs/ad_generator.py
@@ -0,0 +1,70 @@
+import os
+import sys
+
+# Add parent folder to path
+current_folder = os.path.dirname(__file__)
+parent_folder = os.path.dirname(current_folder)
+sys.path.append(parent_folder)
+
+from scraping.web_scraping import WebScraping
+
+
+class AdGenerator ():
+
+    def __init__(self, scraper: WebScraping):
+        self.scraper = scraper
+
+    def create_ad_1(self, ad_id: int, ad_data: dict) -> str:
+        """ Generate ad 1 for pinterest
+        
+        Args:
+            ad_id (int): Id of ad
+            ad_data (dict): Dict with ad data
+                add 1:
+                    product_name (str): Name of product
+                    product_image (str): Image of product
+                    product_price_1 (str): Price of product
+                    product_price_2 (str): Price of product
+                    product_price_3 (str): Price of product
+                    product_price_4 (str): Price of product
+        
+        Returns:
+            str: Path of ad image
+        """
+        
+        # Load ad page
+        if ad_id == 1:
+            ad_page = f"http://localhost:5001/ad-{ad_id}" \
+                f"?product-name={ad_data['product_name']}" \
+                f"&product-image={ad_data['product_image']}" \
+                f"&product-price-1={ad_data['product_price_1']}" \
+                f"&product-price-2={ad_data['product_price_2']}" \
+                f"&product-price-3={ad_data['product_price_3']}" \
+                f"&product-price-4={ad_data['product_price_4']}"
+                
+            self.scraper.set_page(ad_page)
+            self.scraper.refresh_selenium()
+            
+            
+        # Take screenshot
+        current_folder = os.path.dirname(__file__)
+        parent_folder = os.path.dirname(current_folder)
+        static_folder = os.path.join(parent_folder, "static", "imgs")
+        file_path = os.path.join(static_folder, "ad-1.png")
+        self.scraper.screenshot(file_path)
+        
+        return file_path
+    
+    
+if __name__ == "__main__":
+    ad_generator = AdGenerator(WebScraping())
+    ad_data = {
+        "product_name": "Product name",
+        "product_image": "https://picsum.photos/200",
+        "product_price_1": "100",
+        "product_price_2": "200",
+        "product_price_3": "300",
+        "product_price_4": "400",
+    }
+    ad_path = ad_generator.create_ad_1(1, ad_data)
+    print(ad_path)
