4200794599ea847ffc1c7e9856dfd8ac1243c774
daridev <darideveloper@gmail.com>
2023-12-20 12:42:18 -0600

    feat (logs): update script

--- logs.py
+++ logs.py
@@ -1,8 +1,22 @@
 import logging
 
 # logs to file
-logging.basicConfig(filename='.log', format='%(asctime)s - %(filename)s (%(lineno)s) - %(levelname)s - %(message)s')
+logging.basicConfig(
+    filename='.log',
+    format='%(asctime)s - %(filename)s (%(lineno)s) - %(levelname)s - %(message)s',
+)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+# Add both stream handler and file handler
 logger.addHandler(logging.StreamHandler())
+logger.addHandler(logging.FileHandler('.log'))
+
+if __name__ == '__main__':
+    # Example log messages
+    logger.debug("This is a debug message")
+    logger.info("This is an info message")
+    logger.warning("This is a warning message")
+    logger.error("This is an error message")
+    logger.critical("This is a critical message")
